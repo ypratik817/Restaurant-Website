8e06e81d5caea39a22d03f1f2221136cc1724f9b
Pratik Yadav <93317361+ypratik817@users.noreply.github.com>
2022-12-22 18:46:38 +0530

    Create LogIn.php

---
+++ LogIn.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+    <!-- <link rel="icon" href="images/download.jpg"  -->
+    <link rel="stylesheet" href="style.css"
+    <head>
+     <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            background-color: #f0d5fe;
+        }
+    </style>
+    <style>
+        label {
+            cursor: pointer;
+            /* display: inline-block; */
+            /* display: flex; */
+            padding: 1px 6px;
+            text-align: left;
+            width: 85px;
+            vertical-align: top;
+            border: 1px solid black;
+            text-transform: uppercase;
+            background-color: #f0d5fe;
+            font-family: 'Big Shoulders Text', cursive;
+        }
+        label:hover{
+	background: #E7E7E7;
+}
+    
+        input {
+            font-size: inherit;
+        }
+    a{
+	font-family: 'Big Shoulders Text', cursive;
+	color: #000;
+	font-size: 1em;
+	text-transform: uppercase;
+	font-weight: 500;
+}
+ a:hover{
+	background: #E7E7E7;
+}
+</style>
+        <style>
+        body{
+        background-image: url("images/86.jpg");
+        background-size: cover;
+        background-color: #cccccc;
+        transition: width 2s, height 2s, background-color 2s, transform 2s;
+        }
+        ab{
+            background-color: #f90000;
+        }</style>
+    
+    
+
+</head>
+    
+
+
+<center>
+
+    <body>
+        <!-- <a href="#">Home</a>  &nbsp;
+    <a href="#">Menu</a>  &nbsp;
+    <a href="#">Contact</a> -->
+        <br>
+        <!-- <marquee width="12%" direction="right" height="60px">
+            <h1 border: 1px solid black;>TCET ERP &emsp;&emsp;TCET ERP  &emsp;&emsp;TCET ERP</h1>
+            </marquee> -->
+            <h2 align="right"><a href="http://localhost/WEB%20DEV/Zeno1.html#">Home</a></h2>
+            <!-- <img  src="images/76.png" alt="" width="150" height="90" class="center"> -->
+            <h1><a href="https://www.google.com/">Log In/Sign Up</a></h1>
+            
+        <yo>
+        <form action="Thank you.html" method="post">
+            <label>Id :</label>
+            <input type="text" class="form-control" pattern="[a-zA-Z0-9]+" required placeholder="Enter your Id" /><br><br>
+            <label>Password :</label>
+            <input type="text" pattern="[a-zA-Z0-9_@]+" required placeholder="Enter Password" /><br><br>
+            <label>DOB :</label>
+            <input type="date" required><br><br>
+            <label>Telephone :</label>
+            <input type="text" pattern="[0-9+]+" required placeholder="Enter your Telephone no." /><br><br>
+            <label>Email Id :</label>
+            <input type="email" required placeholder="Enter your Email Id" /><br><br>
+            <input type="submit" value="Log in">
+
+        </form>
+        </yo>
+    <br><br>
+    </body>
+</center>
+
+</html>
